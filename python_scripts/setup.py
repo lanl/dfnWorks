ae58c57cc126b231e04cb385b55f8d6a78373e48
Jeffrey De'Haven Hyman <jhyman@lanl.gov>
2017-02-07 11:26:07 -0700

    update setup.py

--- python_scripts/setup.py
+++ python_scripts/setup.py
@@ -2,45 +2,77 @@
 import sys
 import os
 import subprocess
+import argparse
+import glob
+
+def command_line_options():
+    '''Read command lines 
+    -clean: True/False to clean 
+    '''
+    parser = argparse.ArgumentParser(description="Command Line Arguments for dfnWorks")
+    parser.add_argument("-clean", "--clean", default=False, action="store_true",
+              help="Run make clean")
+    options = parser.parse_args()
+    return options
+	
+def remove_batch(name):
+    """ Removes files with wild card *"""
+    print("Removing files:")
+    for fl in glob.glob(name):
+        print fl
+        try:
+	        os.remove(fl)
+        except OSError:
+            pass	
 
 # setup option - performs all compilations necessary before running run_dfnworks.py
-def compile(make_directory_list):
-        for directory in make_directory_list:
-                os.chdir(directory)
-                subprocess.call("make")
-
-# clean option - removes all files not needed in the repository
-def clean(make_directory_list, python_dir):
-        for directory in make_directory_list:
+def compile_exe(directory_list):
+        for directory in directory_list:
                 print directory
                 os.chdir(directory)
-                subprocess.call("rm *.o")
+                subprocess.call("make")
+                print("")
+
+
+# clean option - removes all files not needed in the repository
+def clean(directory_list, python_dir):
+        for directory in directory_list:
+                print directory
+                os.chdir(directory)
+                remove_batch("*.o")
+                print ""
         os.chdir(python_dir)
-        subprocess.call("rm -rf *~")
-        subprocess.call("rm -rf *.pyc")
+        remove_batch("*pyc")
+
 
 # 1. C_uge_correct - make
 # 2. DFN_Mesh_Connectivity_Test - make
 # 3. Particle Tracking - make
 # 4. GenIntElmList - make
+# 5. DFNGen - make
 
-DFN_PATH = '/home/nknapp/dfnworks-main/'
+options=command_line_options()
+
+DFN_PATH = '/home/jhyman/dfnworks/dfnworks-main/'
 C_UGE_PATH = DFN_PATH + 'C_uge_correct/'
 CONNECTIVITY_TEST_PATH = DFN_PATH + 'DFN_Mesh_Connectivity_Test/'
 PARTICLE_TRACKING_PATH = DFN_PATH +'ParticleTracking/'
 GEN_INT_ELM_LIST_PATH = DFN_PATH + 'GenIntElmtList/'
-PYTHON_DIR = DFN_PATH + 'python_scripts/'
+DFNGEN_PATH = DFN_PATH +'DFNGen/DFNC++Version/'
+PYTHON_DIR = DFN_PATH + 'python_scripts/modules'
 
-make_directory_list = []
-make_directory_list.append(C_UGE_PATH)
-make_directory_list.append(CONNECTIVITY_TEST_PATH)
-make_directory_list.append(PARTICLE_TRACKING_PATH)
-make_directory_list.append(GEN_INT_ELM_LIST_PATH)
+directory_list = []
+directory_list.append(C_UGE_PATH)
+directory_list.append(CONNECTIVITY_TEST_PATH)
+directory_list.append(PARTICLE_TRACKING_PATH)
+directory_list.append(DFNGEN_PATH)
+directory_list.append(GEN_INT_ELM_LIST_PATH)
 
-
-#clean(make_directory_list, PYTHON_DIR)
-if (len(sys.argv) == 1):
-	compile(make_directory_list)
+if options.clean:
+    print("Removing *.o and *.pyc files before compiling\n")
+    clean(directory_list, PYTHON_DIR)	
+    print("Compiling executables\n")
+    compile_exe(directory_list)
 else:
-	print 'Invalid arguments to setup script.'
+    compile_exe(directory_list)
 	
