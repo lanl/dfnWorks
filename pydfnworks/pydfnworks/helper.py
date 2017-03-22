7519f241aae1bb762ffb766479667f5fcdb758f6
Nathaniel Knapp <nknapp@lanl.gov>
2017-03-22 11:08:19 -0600

    added full_mesh_vol_area.uge to ignore list for cleanup after pflotran

--- pydfnworks/pydfnworks/helper.py
+++ pydfnworks/pydfnworks/helper.py
@@ -8,7 +8,7 @@ def cleanup_files(self, output_dir):
     gen_exclude_list = ['intersections', 'polys', 'radii', 'DFN_output.txt', 'aperture.dat',
                         'families.dat', 'params.txt', 'perm.dat', 'poly_info.dat', 'triple_points.dat']  
     lagrit_exclude_list = ['materialid.dat', 'allboundaries.zone', 'full_mesh.inp', 'full_mesh.uge', 'parameters', 'pboundary_', 'tri_fracture']
-    pflotran_exclude_list = ['cellinfo.dat', 'darcyvel.dat']
+    pflotran_exclude_list = ['cellinfo.dat', 'darcyvel.dat', 'full_mesh_vol_area.uge']
     dir_name_list = ['DFN_generator', 'LaGriT', 'PFLOTRAN']
     exclude_list = gen_exclude_list + lagrit_exclude_list + pflotran_exclude_list +  dir_name_list
     delete_list = ['output_files.txt', 'dfn_explicit-cellinfo', 'dfn_explicit-darcyvel', 'dfn_explicit-mas.dat', 'convert_uge_params.txt', 'logx3dgen', 'outx3dgen', '-e']
