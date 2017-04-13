9c927ff241be26eb37169594e32ec457f74fe9ab
Nathaniel Knapp <nknapp@lanl.gov>
2017-04-13 09:57:49 -0600

    .gitignore now working

--- DFNGen/domain.h
+++
@@ -1,8 +0,0 @@
-#ifndef _domain_h_
-#define _domain_h_
-#include "structures.h"
-
-bool domainTruncation(Poly &newPoly, double *domainSize);
-void printPoints(std::vector<double> &point);
-
-#endif
--- DFNGen/removeFractures.h
+++
@@ -1,8 +0,0 @@
-#ifndef _REMOVEFRACTURES_H
-#define _REMOVEFRACTURES_H
-
-#include "structures.h"
-#include <vector>
-
-void removeFractures(double minSize, std::vector<Poly> &acceptedPolys, std::vector<IntPoints> &intPts, std::vector<Point> triplePoints, Stats  &pstats);
-#endif
