e657f82616b524c0c5d3148779f551ae7529a6f0
Nathaniel Knapp <nknapp@lanl.gov>
2017-04-18 10:39:18 -0600

    correct fix_paths.py

--- pydfnworks/bin/fix_paths.py
+++ pydfnworks/bin/fix_paths.py
@@ -7,7 +7,6 @@ def replace(old, new):
     subprocess.call("find ../../tests/ -type f -print0 | xargs -0 sed -i -e 's@" + old + "@" + new + "@g'", shell=True)
     subprocess.call("find ../pydfnworks/ -type f -print0 | xargs -0 sed -i -e 's@" + old + "@" + new + "@g'", shell=True)
 
-old = '/home/nknapp'
-new = 'DUMMY'
-#new = os.getcwd().split('/dfnworks-main')[0]
+old = 'DUMMY'
+new = os.getcwd().split('/dfnworks-main')[0]
 replace(old, new)
