6e42fd18aa15b14093a2d9af046a66a1779c1096
Jeffrey De'Haven Hyman <jhyman@lanl.gov>
2017-04-24 14:43:28 -0600

    graph information now output from DFNGen.

--- DFNGen/tmp.cpp
+++
@@ -1,7 +0,0 @@
-
-    std::ofstream bottomFile; 
-    std::string bottomFileName = "bottom.txt"; 
-    bottomFile.open(bottomFileName.c_str(), std::ofstream::out | std::ofstream::trunc);
-    checkIfOpen(bottomFile, bottomFileName);
-
-
