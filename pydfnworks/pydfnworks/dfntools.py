f3028cb9abf4ce484717aa7509ede5609397b122
Jeffrey De'Haven Hyman <jhyman@lanl.gov>
2017-08-07 17:55:01 -0600

    correct indentation in dfntools.py

--- pydfnworks/pydfnworks/dfntools.py
+++ pydfnworks/pydfnworks/dfntools.py
@@ -1,32 +1,32 @@
 """ Tools for dfnworks data """
 
 class Frozen(object):
-	"""
-	Prevents adding new attributes to classes once _freeze() is called on the class.
-	"""
-	frozen = False
+    """
+    Prevents adding new attributes to classes once _freeze() is called on the class.
+    """
+    frozen = False
 
-	def __setattr__(self, key, value):
-	    """
+    def __setattr__(self, key, value):
+        """
             Set attribute to a value.
             Args:
                 key (string): the key of the attribute being set.
                 value : the value of the attribute being set. 
-            """
-            if not self.frozen or hasattr(self, key):
-		        object.__setattr__(self, key, value)
-	        else:
-		        raise AttributeError(str(key) + ' is not a valid attribute for ' + self._class__.__name__)
+        """
+        if not self.frozen or hasattr(self, key):
+            object.__setattr__(self, key, value)
+        else:
+            raise AttributeError(str(key) + ' is not a valid attribute for ' + self._class__.__name__)
 
-	def _freeze(self):
-            """
-            Prevents adding new attributes to a class.
-            """
-            self.frozen = True
+    def _freeze(self):
+        """
+         Prevents adding new attributes to a class.
+        """
+        self.frozen = True
 
-	def _unfreeze(self):
-	    """
-            Allows adding new attributes to classes.
-            """
-            self.frozen = False
+    def _unfreeze(self):
+        """
+        Allows adding new attributes to classes.
+        """
+        self.frozen = False
 
