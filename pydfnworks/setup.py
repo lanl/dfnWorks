8fe02a03d28b1ba051103f16b8e17a724abbf38c
Jeffrey Hyman <hymanjd@gmail.com>
2018-04-03 11:26:07 -0600

    remove knapp refernecs and updated to 2.1 in setup.py

--- pydfnworks/setup.py
+++ pydfnworks/setup.py
@@ -6,7 +6,7 @@ import os
 #os.system('mv ~/.local ~/local_save')
 
 setup(name='pydfnworks',
-      version='2.0',
+      version='2.1',
       description='Python methods for running dfnWorks',
       url='TBD',
       author='Jeffrey Hyman, Satish Karra, Nathaniel Knapp, Nataliia Makedonska',
@@ -22,5 +22,4 @@ setup(name='pydfnworks',
       test_suite='nose.collector',
       tests_require=['nose'],
       scripts=['bin/test_integrated.py', 'bin/compile.py', 'bin/test.py', 'bin/run.py', 'bin/run_explicit.py'],
-      dependency_links=['https://github.com/deknapp/testdfn/tarball/master#egg-package-1.0'],
       zip_safe=False)
