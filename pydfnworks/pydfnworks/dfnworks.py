216f5a3d22691db71c69020f87d9beea60908e7b
Jeffrey Hyman <hymanjd@gmail.com>
2019-01-22 08:15:57 -0700

    update dfnworks.py

--- pydfnworks/pydfnworks/dfnworks.py
+++ pydfnworks/pydfnworks/dfnworks.py
@@ -38,7 +38,7 @@ class dfnworks(Frozen):
     from flow import dfn_flow
     from transport import dfn_trans
     # Specific functions
-    from general_functions import commandline_options, dump_time, print_run_time 
+    from general_functions import dump_time, print_run_time 
     from dfn2graph import create_graph, k_shortest_paths_backbone, dump_json_graph, load_json_graph, plot_graph, greedy_edge_disjoint, dump_fractures 
    
     from gen_input import check_input
