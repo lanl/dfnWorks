9f87651752ca989315174d1b70708e95e5f243f3
Jeffrey Hyman <hymanjd@gmail.com>
2019-01-31 14:08:15 -0700

    changed tests to examples'

--- pydfnworks/bin/fix_paths.py
+++ pydfnworks/bin/fix_paths.py
@@ -5,7 +5,7 @@ import sys, os
 
 def replace(old, new):
     subprocess.call("find ../ -type f -print0 | xargs -0 sed -i -e 's@" + old + "@" + new + "@g'", shell=True)
-    subprocess.call("find ../../tests -type f -print0 | xargs -0 sed -i -e 's@" + old + "@" + new + "@g'", shell=True)
+    subprocess.call("find ../../examples -type f -print0 | xargs -0 sed -i -e 's@" + old + "@" + new + "@g'", shell=True)
 
 old = 'DUMMY/'
  
