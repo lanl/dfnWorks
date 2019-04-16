21797d39e90822bb744dfb11c1ea49118e144b13
Jeffrey Hyman <hymanjd@gmail.com>
2019-04-16 15:18:52 -0600

    updated pruning example

--- examples/pruning/run_prune_explicit_part1.py
+++ examples/pruning/run_prune_explicit_part1.py
@@ -16,15 +16,21 @@ define_paths()
 main_time = time()
 DFN = create_dfn()
 
+# Create network but don't mesh it
 DFN.make_working_directory()
 DFN.check_input()
 DFN.create_network()
 DFN.mesh_network(visual_mode=True)
 
+# Create a graph based on the DFN
 G = DFN.create_graph("fracture", "left", "right")
+# Plot the graph based on the DFN
 DFN.plot_graph(G,output_name="full_dfn")
+# Isolate the 2-Core of the graph
 H = nx.k_core(G,2)
+# Dump out fractures in the 2-Core
 DFN.dump_fractures(H,"2_core.dat") 
+# plot the 2 core of the graph
 DFN.plot_graph(H,output_name="dfn_2_core")
 
 main_elapsed = time() - main_time
--- examples/pruning/run_prune_explicit_part2.py
+++ examples/pruning/run_prune_explicit_part2.py
@@ -18,8 +18,10 @@ DFN = create_dfn()
 
 DFN.make_working_directory()
 
+# Mesh the network based on the 2 core, assigned by the command line file
 DFN.mesh_network(prune=True)
 
+# run flow and transport on the 2-Core DFN
 DFN.dfn_flow()
 DFN.dfn_trans()
 
