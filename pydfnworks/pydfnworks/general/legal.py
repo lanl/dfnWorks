491c55c1805065e0083b87defe153c1208bb3ed7
Daniel Livingston <livingston@lanl.gov>
2019-08-21 10:18:53 -0600

    Update legal.py

--- pydfnworks/pydfnworks/general/legal.py
+++ pydfnworks/pydfnworks/general/legal.py
@@ -55,16 +55,23 @@ LA-CC-17-027
 (or copyright) 2018 Triad National Security, LLC. All rights reserved.
  
 This program was produced under U.S. Government contract 89233218CNA000001
- for Los Alamos National Laboratory (LANL), which is operated by Triad 
- National Security, LLC for the U.S. Department of Energy/National Nuclear
-  Security Administration.
- 
+for Los Alamos National Laboratory (LANL), which is operated by Triad 
+National Security, LLC for the U.S. Department of Energy/National Nuclear
+Security Administration.
+
+This is free software; you can redistribute it and/or modify it under the
+terms of the GNU Lesser General Public License as published by the Free
+Software Foundation; either version 3.0 of the License, or (at your option)
+any later version. If software is modified to produce derivative works,
+such modified software should be clearly marked, so as not to confuse it with
+the version available from LANL.
+
 All rights in the program are reserved by Triad National Security, LLC, 
 and the U.S. Department of Energy/National Nuclear Security Administration.
- The Government is granted for itself and others acting on its behalf a 
- nonexclusive, paid-up, irrevocable worldwide license in this material 
- to reproduce, prepare derivative works, distribute copies to the public,
-  perform publicly and display publicly, and to permit others to do so.
+The Government is granted for itself and others acting on its behalf a 
+nonexclusive, paid-up, irrevocable worldwide license in this material 
+to reproduce, prepare derivative works, distribute copies to the public,
+perform publicly and display publicly, and to permit others to do so.
  
 
  The U.S. Government has rights to use, reproduce, and distribute this software.  
