7a75702eeee9557a01884ceefb35d0cda7bd5074
Jeffrey Hyman <hymanjd@gmail.com>
2022-08-18 14:19:00 -0600

    test driver for TPL

---
+++ examples/TPL_obj/driver.py
@@ -0,0 +1,34 @@
+#"""
+#   :synopsis: Driver run file for TPL example 
+#   :version: 2.0
+#   :maintainer: Jeffrey Hyman
+#.. moduleauthor:: Jeffrey Hyman <jhyman@lanl.gov>
+#"""
+
+from pydfnworks import * 
+
+import os
+
+jobname = os.getcwd() + "/tpl_obj_output"
+DFN = DFNWORKS(jobname)
+DFN.params['domainSize']['value'] = [10,10,10]
+DFN.params["h"]["values"] = 0.1
+
+DFN.add_fracture_family(shape = "rect", distribution = "constant", constant = 1, 
+    kappa = 10, phi = 0, theta = 0, p32 = 1)
+
+DFN.add_fracture_family(shape = "rect", distribution = "tpl", alpha = 3,
+    min_radius = 1, max_radius = 10,
+    kappa = 10, phi = 0, theta = 0, p32 = 4)
+DFN.print_family_information(1)    
+ 
+DFN.make_working_directory(delete = True)
+DFN.check_input()
+
+
+# DFN.create_network()
+# DFN.output_report()
+# DFN.mesh_network(coarse_factor=10)
+
+# DFN.dfn_flow()
+# DFN.dfn_trans()
