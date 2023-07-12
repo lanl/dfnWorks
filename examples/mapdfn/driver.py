6d95a3436f920d56981b9a9ece2f7319e991d52e
Jeffrey Hyman <you@ejhyman@lanl.gov>
2023-07-12 14:17:42 -0600

    mapdfn initial complete

--- examples/mapdfn/driver.py
+++ examples/mapdfn/driver.py
@@ -12,14 +12,13 @@ from pydfnworks import *
 src_path = os.getcwd()
 
 jobname = f"{src_path}/output"  
-pflotran_card = f"{src_path}/cpm_pflotran.in" 
         
-DFN = DFNWORKS(jobname, dfnFlow_file = pflotran_card)
+DFN = DFNWORKS(jobname)
 
 DFN.params['domainSize']['value'] = [1000.0, 1000.0, 1000.0]
 DFN.params['domainSizeIncrease']['value'] = [100, 100, 100]
 DFN.params['h']['value'] = 3.0
-DFN.params['stopCondition']['value'] = 1 #0 for nPoly
+DFN.params['stopCondition']['value'] = 1 
 DFN.params['seed']['value'] = 1 
 DFN.params['boundaryFaces']['value'] = [1,1,0,0,0,0]
 DFN.params['visualizationMode']['value'] = True
@@ -27,70 +26,58 @@ DFN.params['seed']['value'] = 200 #seed for random generator 0 seeds off clock
 DFN.params['ignoreBoundaryFaces']['value'] = False
 DFN.params['boundaryFaces']['value'] = [1,1,0,0,0,0]
 
-# DFN.add_fracture_family(shape="ell",
-#                         distribution="tpl",
-#                         probability = 0.219, #unnecessary unless stop condition = 1
-#                         p32 = 0.0099,
-#                         beta_distribution = 1,
-#                         beta = 0,
-#                         number_of_points = 12,
-#                         kappa=9.41,
-#                         theta=1.65*180./3.14,
-#                         phi=4.78*180./3.14,
-#                         alpha=2.4,
-#                         min_radius=30.0,
-#                         max_radius=564.0,
-#                         hy_variable='permeability',
-#                         hy_function='semi-correlated',
-#                         hy_params={
-#                             "alpha": 1e-13,
-#                             "beta": 0.9,
-#                             "sigma": 1.0
-#                         })
+DFN.add_fracture_family(shape="ell",
+                        distribution="tpl",
+                        p32 = 0.0099,
+                        number_of_points = 12,
+                        kappa=9.41,
+                        theta=95,
+                        phi=274,
+                        alpha=2.4,
+                        min_radius=30.0,
+                        max_radius=564.0,
+                        hy_variable='permeability',
+                        hy_function='correlated',
+                        hy_params={
+                            "alpha": 1e-12,
+                            "beta": 0.9,
+                        })
 
 
-# DFN.add_fracture_family(shape="ell",
-#                         distribution="tpl",
-#                         probability = 0.250,
-#                         p32 = 0.0113,
-#                         beta_distribution = 1,
-#                         beta = 0,
-#                         number_of_points = 12,
-#                         kappa=8.3,
-#                         theta=1.57*180./3.14,
-#                         phi=3.14*180./3.14,
-#                         alpha=2.4,
-#                         min_radius=30.0,
-#                         max_radius=564.0,
-#                         hy_variable='permeability', #you can play around with this
-#                         hy_function='semi-correlated',
-#                         hy_params={
-#                             "alpha": 1e-13,
-#                             "beta": 0.9,
-#                             "sigma": 1.0
-#                         })
+DFN.add_fracture_family(shape="ell",
+                        distribution="tpl",
+                        p32 = 0.0113,
+                        number_of_points = 12,
+                        kappa=8.3,
+                        theta=90,
+                        phi=180,
+                        alpha=2.4,
+                        min_radius=30.0,
+                        max_radius=564.0,
+                        hy_variable='permeability', 
+                        hy_function='correlated',
+                        hy_params={
+                            "alpha": 1e-12,
+                            "beta": 0.9,
+                        })
 
 
-# DFN.add_fracture_family(shape="ell",
-#                         distribution="tpl",
-#                         probability = 0.531,
-#                         p32 = 0.0240,
-#                         beta_distribution = 1,
-#                         beta = 0,
-#                         number_of_points = 12,
-#                         kappa=5.7,
-#                         theta=2.95*180./3.14,
-#                         phi=2.62*180./3.14,
-#                         alpha=2.4,
-#                         min_radius=30.0,
-#                         max_radius=564.0,
-#                         hy_variable='permeability',
-#                         hy_function='semi-correlated',
-#                         hy_params={
-#                             "alpha": 1e-13,
-#                             "beta": 0.9,
-#                             "sigma": 1.0
-#                         })
+DFN.add_fracture_family(shape="ell",
+                        distribution="tpl",
+                        p32 = 0.0240,
+                        number_of_points = 12,
+                        kappa=5.7,
+                        theta=169,
+                        phi=150,
+                        alpha=2.4,
+                        min_radius=30.0,
+                        max_radius=564.0,
+                        hy_variable='permeability',
+                        hy_function='correlated',
+                        hy_params={
+                            "alpha": 1e-12,
+                            "beta": 0.9,
+                        })
 
 DFN.add_user_fract(shape='ell',
                     radii=600,
@@ -126,14 +113,17 @@ DFN.make_working_directory(delete=True)
 DFN.print_domain_parameters()
 DFN.check_input()
 DFN.create_network()
-DFN.dump_hydraulic_values()
-DFN.mesh_network()
+#DFN.dump_hydraulic_values()
+#DFN.mesh_network()
 
 #DFN.dfn_flow()
 mat_perm = 1e-16
 mat_por = 0.1
 cell_size = 20
 DFN.mapdfn_ecpm(mat_perm, mat_por, cell_size)
+
+DFN.dfnFlow_file = f"{src_path}/cpm_pflotran.in"
+DFN.local_dfnFlow_file = f"cpm_pflotran.in"
 DFN.pflotran()
 
 DFN.dfnFlow_file = f"{src_path}/cpm_transport.in"
