b9a99893eafbcc7a2d9ad84e5989ec73c0291058
Jeffrey Hyman <you@ejhyman@lanl.gov>
2023-11-01 15:34:30 -0400

    I think rFram is working

--- pydfnworks/pydfnworks/general/dfnworks.py
+++ pydfnworks/pydfnworks/general/dfnworks.py
@@ -60,7 +60,7 @@ class DFNWORKS():
     from pydfnworks.dfnGen.meshing.mesh_dfn.mesh_dfn_helper import inp2gmv, create_mesh_links, inp2vtk_python, gather_mesh_information
     from pydfnworks.dfnGen.meshing.mesh_dfn.poisson_driver import create_lagrit_parameters_file
     from pydfnworks.dfnGen.meshing.mesh_dfn.lagrit_merge_mesh import create_merge_poly_scripts, create_final_merge_script
-    from pydfnworks.dfnGen.meshing.mesh_dfn.run_meshing import mesh_fractures_header,merge_network
+    from pydfnworks.dfnGen.meshing.mesh_dfn.run_meshing import mesh_fractures_header,merge_network, check_for_missing_edges
     from pydfnworks.dfnGen.meshing.mesh_dfn.prune_mesh_scripts import edit_intersection_files, clean_up_files_after_prune
     from pydfnworks.dfnGen.meshing.add_attribute_to_mesh import add_variable_to_mesh
 
