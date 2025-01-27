9b3f44021c7b54f0b3264e13c86d4c8afd51e8e6
Jeffrey De'Haven Hyman <jhyman@lanl.gov>
2025-01-27 12:34:54 -0500

    log file name

--- pydfnworks/pydfnworks/general/dfnworks.py
+++ pydfnworks/pydfnworks/general/dfnworks.py
@@ -146,10 +146,8 @@ class DFNWORKS():
                  cell_based_aperture=False,
                  store_polygon_data=True,
                  pickle_file=None,
-                 log_filename="dfnWorks",
+                 log_filename=None,
                  log_time=True):
-                 #log_filename=None,
-                 #log_time=False):
         
         ## initialize variables 
         self.num_frac = int
