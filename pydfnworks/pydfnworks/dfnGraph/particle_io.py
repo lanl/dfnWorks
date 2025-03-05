7ed4298436193481095af46f3c82be858b6e67d5
Jeffrey De'Haven Hyman <jhyman@lanl.gov>
2025-03-05 13:28:31 -0500

    added initial velocity to partime

--- pydfnworks/pydfnworks/dfnGraph/particle_io.py
+++ pydfnworks/pydfnworks/dfnGraph/particle_io.py
@@ -174,15 +174,16 @@ def gather_particle_info(particles):
     total_times = np.zeros_like(adv_times)
     length = np.zeros_like(adv_times)
     beta = np.zeros_like(adv_times)
-
+    initial_velocity = np.zeros_like(adv_times) 
     for i, particle in enumerate(particles):
         adv_times[i] = particle.advect_time
         md_times[i] = particle.matrix_diffusion_time
         total_times[i] = particle.total_time
         length[i] = particle.length
         beta[i] = particle.beta
+        initial_velocity[i] = particle.velocity[0]
 
-    return adv_times, md_times, total_times, length, beta, stuck_particle_cnt
+    return adv_times, md_times, total_times, length, beta, initial_velocity, stuck_particle_cnt
 
 
 def dump_particle_info(particles, partime_file, frac_id_file, format):
@@ -210,16 +211,16 @@ def dump_particle_info(particles, partime_file, frac_id_file, format):
                 Number of particles that do not exit the domain
 
     """
-    adv_times, md_times, total_times, length, beta, stuck_cnt = gather_particle_info(
+    adv_times, md_times, total_times, length, beta, initial_velocity, stuck_cnt = gather_particle_info(
         particles)
 
     if format == 'ascii':
         filename = f"{partime_file}.dat"
         local_print_log(f"--> Writing Data to files: {filename}")
         # Write Header
-        header = "Advective time [s],Matrix Diffusion time [s],Total travel time [s],Pathline length [m],Beta (s m^-1)"
+        header = "Advective time [s],Matrix Diffusion time [s],Total travel time [s],Pathline length [m],Beta (s m^-1),Initial Velocity"
         np.savetxt(filename,
-                   np.c_[adv_times, md_times, total_times, length, beta],
+                   np.c_[adv_times, md_times, total_times, length, beta, ],
                    delimiter=",",
                    header=header)
 
@@ -251,6 +252,10 @@ def dump_particle_info(particles, partime_file, frac_id_file, format):
             dataset_name = "Beta [s m^-1]"
             h5dset = f5file.create_dataset(dataset_name, data=beta)
 
+            dataset_name = "Initial Velocity [m s^-1]"
+            h5dset = f5file.create_dataset(dataset_name, data=initial_velocity)
+
+
         if frac_id_file:
             filename = f"{frac_id_file}.hdf5"
             local_print_log(f"--> Writing fractures visted to file: {filename}")
@@ -272,7 +277,8 @@ def dump_particle_info(particles, partime_file, frac_id_file, format):
             'Matrix Diffusion time [s]': md_times,
             'Total travel time [s]': total_times,
             'Pathline length [m]': length,
-            'Beta (s/m)': beta
+            'Beta (s/m)': beta,
+            'Initial Velocity (m/s)': initial_velocity
         }
         pickle.dump(data_dict, open(filename, "wb"))
 
