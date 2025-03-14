b33d6eb238c991378d8efdbb6b590f510f7efcc7
ehinrichs <54997013+ehinrichs@users.noreply.github.com>
2025-03-14 14:19:18 -0600

    Updates to clean up

--- examples/run_examples.py
+++ examples/run_examples.py
@@ -20,7 +20,7 @@ home = os.getcwd()
 
 try:
     examples_dirs = [sys.argv[1]]
-    verbose = True 
+    verbose = True
 except:
     examples_dirs = glob.glob("*")
     for d in examples_dirs:
@@ -41,6 +41,12 @@ for i,d in enumerate(examples_dirs):
     try:
         print(d)
         os.chdir(d)
+        if os.path.isdir('output'):
+            shutil.rmtree('output')
+            print(f"--> Removing output directory from {d}")
+        if os.path.isfile('output.log'):
+            os.remove('output.log')
+            print(f"--> Removing output.log file from {d}")
         driver_file = glob.glob("*py")
         if verbose:
             cmd = f"python {driver_file[0]}"
@@ -52,9 +58,13 @@ for i,d in enumerate(examples_dirs):
         toc = timeit.default_timer()
         elapsed = toc - tic
         print(f"--> Time required {elapsed:0.2f} seconds\n")
-        cleanup_dirs = glob.glob("output*")
-        for dir_to_delete in cleanup_dirs:
-            shutil.rmtree(dir_to_delete)
+        print(f"--> Cleaning up outputs")
+        if os.path.isdir('output'):
+            shutil.rmtree('output')
+            print(f"--> Removing output directory from {d}")
+        if os.path.isfile('output.log'):
+            os.remove('output.log')
+            print(f"--> Removing output.log file from {d}")
         os.chdir(home)
         #df_index = len(df) + 1
         #print(df_index, i)
@@ -62,7 +72,7 @@ for i,d in enumerate(examples_dirs):
 
     except Exception as error:
         print(f"--> {d} failed")
-        print(f"--> error {error}") 
+        print(f"--> error {error}")
         df.loc[i+1,:] = [d, 'Fail', elapsed, error]
         os.chdir(home)
 
